e106cbfc263316209f25e0aced07ef8f3db51f47
Si13n7 <si13n7@live.de>
2015-11-14 15:04:59 +0100

    Fix for an layout issue in Windows 10 Threshold 2.

--- AppsLauncher/MainForm.Designer.cs
+++ AppsLauncher/MainForm.Designer.cs
@@ -48,7 +48,17 @@
             this.label1 = new System.Windows.Forms.Label();
             this.settingsBtn = new System.Windows.Forms.Button();
             this.startBtn = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel5 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
             this.appMenu.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel5.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.tableLayoutPanel4.SuspendLayout();
             this.SuspendLayout();
             // 
             // appMenu
@@ -144,11 +154,12 @@
             // 
             // searchBox
             // 
+            this.searchBox.Dock = System.Windows.Forms.DockStyle.Fill;
             this.searchBox.Font = new System.Drawing.Font("Comic Sans MS", 8.25F);
             this.searchBox.ForeColor = System.Drawing.SystemColors.GrayText;
-            this.searchBox.Location = new System.Drawing.Point(14, 53);
+            this.searchBox.Location = new System.Drawing.Point(3, 3);
             this.searchBox.Name = "searchBox";
-            this.searchBox.Size = new System.Drawing.Size(270, 23);
+            this.searchBox.Size = new System.Drawing.Size(276, 23);
             this.searchBox.TabIndex = 2;
             this.searchBox.Text = "S E A R C H";
             this.searchBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -159,8 +170,9 @@
             // 
             // addBtn
             // 
+            this.addBtn.Dock = System.Windows.Forms.DockStyle.Top;
             this.addBtn.Image = global::AppsLauncher.Properties.Resources.add_a_13;
-            this.addBtn.Location = new System.Drawing.Point(263, 18);
+            this.addBtn.Location = new System.Drawing.Point(258, 3);
             this.addBtn.Name = "addBtn";
             this.addBtn.Size = new System.Drawing.Size(21, 21);
             this.addBtn.TabIndex = 1;
@@ -172,12 +184,13 @@
             // appsBox
             // 
             this.appsBox.ContextMenuStrip = this.appMenu;
+            this.appsBox.Dock = System.Windows.Forms.DockStyle.Fill;
             this.appsBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.appsBox.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.appsBox.FormattingEnabled = true;
-            this.appsBox.Location = new System.Drawing.Point(14, 18);
+            this.appsBox.Location = new System.Drawing.Point(3, 3);
             this.appsBox.Name = "appsBox";
-            this.appsBox.Size = new System.Drawing.Size(243, 21);
+            this.appsBox.Size = new System.Drawing.Size(249, 21);
             this.appsBox.Sorted = true;
             this.appsBox.TabIndex = 0;
             this.appsBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.appsBox_KeyPress);
@@ -185,38 +198,41 @@
             // appsCount
             // 
             this.appsCount.BackColor = System.Drawing.Color.Transparent;
+            this.appsCount.Dock = System.Windows.Forms.DockStyle.Fill;
             this.appsCount.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.appsCount.ForeColor = System.Drawing.SystemColors.GrayText;
-            this.appsCount.Location = new System.Drawing.Point(152, 116);
+            this.appsCount.Location = new System.Drawing.Point(144, 0);
             this.appsCount.Name = "appsCount";
-            this.appsCount.Size = new System.Drawing.Size(132, 14);
+            this.appsCount.Size = new System.Drawing.Size(135, 21);
             this.appsCount.TabIndex = 6;
             this.appsCount.Text = "0 apps found!";
-            this.appsCount.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.appsCount.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // label1
             // 
             this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label1.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.GrayText;
-            this.label1.Location = new System.Drawing.Point(14, 116);
+            this.label1.Location = new System.Drawing.Point(3, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(131, 14);
+            this.label1.Size = new System.Drawing.Size(135, 21);
             this.label1.TabIndex = 5;
             this.label1.Text = "www.si13n7.com";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // settingsBtn
             // 
             this.settingsBtn.BackColor = System.Drawing.SystemColors.Control;
+            this.settingsBtn.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.settingsBtn.FlatAppearance.BorderSize = 0;
             this.settingsBtn.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.Highlight;
             this.settingsBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.settingsBtn.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.settingsBtn.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.settingsBtn.Location = new System.Drawing.Point(152, 88);
+            this.settingsBtn.Location = new System.Drawing.Point(144, 4);
             this.settingsBtn.Name = "settingsBtn";
-            this.settingsBtn.Size = new System.Drawing.Size(132, 20);
+            this.settingsBtn.Size = new System.Drawing.Size(135, 20);
             this.settingsBtn.TabIndex = 4;
             this.settingsBtn.Text = "Settings";
             this.settingsBtn.UseVisualStyleBackColor = false;
@@ -225,18 +241,102 @@
             // startBtn
             // 
             this.startBtn.BackColor = System.Drawing.SystemColors.Control;
+            this.startBtn.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.startBtn.FlatAppearance.BorderSize = 0;
             this.startBtn.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.Highlight;
             this.startBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.startBtn.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.startBtn.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.startBtn.Location = new System.Drawing.Point(14, 88);
+            this.startBtn.Location = new System.Drawing.Point(3, 4);
             this.startBtn.Name = "startBtn";
-            this.startBtn.Size = new System.Drawing.Size(131, 20);
+            this.startBtn.Size = new System.Drawing.Size(135, 20);
             this.startBtn.TabIndex = 3;
             this.startBtn.Text = "Run";
             this.startBtn.UseVisualStyleBackColor = false;
             this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 5F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel5, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel4, 1, 3);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 8F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 26.31579F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 26.31579F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 26.31579F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 21.05263F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(298, 134);
+            this.tableLayoutPanel1.TabIndex = 7;
+            // 
+            // tableLayoutPanel5
+            // 
+            this.tableLayoutPanel5.ColumnCount = 2;
+            this.tableLayoutPanel5.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel5.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel5.Controls.Add(this.appsCount, 1, 0);
+            this.tableLayoutPanel5.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel5.Location = new System.Drawing.Point(8, 110);
+            this.tableLayoutPanel5.Name = "tableLayoutPanel5";
+            this.tableLayoutPanel5.RowCount = 1;
+            this.tableLayoutPanel5.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel5.Size = new System.Drawing.Size(282, 21);
+            this.tableLayoutPanel5.TabIndex = 5;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.BackColor = System.Drawing.Color.Transparent;
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 27F));
+            this.tableLayoutPanel2.Controls.Add(this.addBtn, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.appsBox, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(8, 11);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(282, 27);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.BackColor = System.Drawing.Color.Transparent;
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.searchBox, 0, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(8, 44);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(282, 27);
+            this.tableLayoutPanel3.TabIndex = 3;
+            // 
+            // tableLayoutPanel4
+            // 
+            this.tableLayoutPanel4.ColumnCount = 2;
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel4.Controls.Add(this.startBtn, 0, 0);
+            this.tableLayoutPanel4.Controls.Add(this.settingsBtn, 1, 0);
+            this.tableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel4.Location = new System.Drawing.Point(8, 77);
+            this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+            this.tableLayoutPanel4.RowCount = 1;
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(282, 27);
+            this.tableLayoutPanel4.TabIndex = 4;
             // 
             // MainForm
             // 
@@ -245,20 +345,14 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Highlight;
             this.BackgroundImage = global::AppsLauncher.Properties.Resources.diagonal_pattern;
-            this.ClientSize = new System.Drawing.Size(298, 136);
-            this.Controls.Add(this.appsCount);
-            this.Controls.Add(this.settingsBtn);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.searchBox);
-            this.Controls.Add(this.appsBox);
-            this.Controls.Add(this.addBtn);
-            this.Controls.Add(this.startBtn);
+            this.ClientSize = new System.Drawing.Size(298, 134);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Font = new System.Drawing.Font("Tahoma", 8.25F);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.HelpButton = true;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(314, 175);
+            this.MinimumSize = new System.Drawing.Size(314, 172);
             this.Name = "MainForm";
             this.Opacity = 0D;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -271,8 +365,13 @@
             this.Shown += new System.EventHandler(this.MainForm_Shown);
             this.DragEnter += new System.Windows.Forms.DragEventHandler(this.MainForm_DragEnter);
             this.appMenu.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel5.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.tableLayoutPanel4.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -296,6 +395,11 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button settingsBtn;
         private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel5;
     }
 }
 
