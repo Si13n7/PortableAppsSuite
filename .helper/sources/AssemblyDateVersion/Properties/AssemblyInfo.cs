867bc3200136d7e18a1307433a20328a23ca2dd0
Si13n7 <si13n7@live.de>
2016-04-06 17:05:50 +0200

    New auto generating version format.

--- .helper/sources/AssemblyDateVersion/Properties/AssemblyInfo.cs
+++ .helper/sources/AssemblyDateVersion/Properties/AssemblyInfo.cs
@@ -1,18 +1,18 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Portable Apps Suite Updater")]
+[assembly: AssemblyTitle("AssemblyVersionJumper")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Si13n7 Dev. ®")]
-[assembly: AssemblyProduct("Updater")]
+[assembly: AssemblyProduct("AssemblyVersionJumper")]
 [assembly: AssemblyCopyright("Copyright © Si13n7 Dev. ® 2016")]
-[assembly: AssemblyTrademark("Si13n7 Dev. ®")]
+[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("a0969572-f61a-453b-9043-e9d1fa8e8240")]
+[assembly: Guid("948dbd6d-121f-4ff7-ad36-7824f078369b")]
 
-[assembly: AssemblyVersion("16.4.6.*")]
+[assembly: AssemblyVersion("1.0.0.*")]
--- AppsDownloader/Properties/AssemblyInfo.cs
+++ AppsDownloader/Properties/AssemblyInfo.cs
@@ -21,4 +21,4 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("a3418f24-e739-45c2-b31e-f50f4124e600")]
 
-[assembly: AssemblyVersion("1.2.6.*")]
+[assembly: AssemblyVersion("16.4.6.*")]
