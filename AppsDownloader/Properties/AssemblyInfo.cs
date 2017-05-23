f84713671c89e0b3f31cc8bb0bc3d10c4bc1896e
Si13n7 <si13n7@live.de>
2017-05-23 10:06:00 +0200

    Update snapshots.

--- AppsDownloader/Properties/AssemblyInfo.cs
+++ AppsDownloader/Properties/AssemblyInfo.cs
@@ -20,4 +20,4 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("a3418f24-e739-45c2-b31e-f50f4124e600")]
 
-[assembly: AssemblyVersion("17.5.22.*")]
+[assembly: AssemblyVersion("17.5.23.*")]
