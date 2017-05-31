e7a8b39e7f84e4b83e7bc2e3498f2fa53d112a9c
Si13n7 <si13n7@live.de>
2017-05-31 17:11:22 +0200

    Update snapshots.

--- AppsLauncher/Properties/AssemblyInfo.cs
+++ AppsLauncher/Properties/AssemblyInfo.cs
@@ -19,4 +19,4 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("ee806ac7-2a8b-4d79-8634-adbdbba0aebf")]
 
-[assembly: AssemblyVersion("17.5.26.*")]
+[assembly: AssemblyVersion("17.5.31.*")]
