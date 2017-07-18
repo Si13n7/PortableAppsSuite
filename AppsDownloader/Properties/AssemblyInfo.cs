9071e652be2adb611aef5159000ae7b7cdb4ee89
Si13n7 <si13n7@live.de>
2017-07-18 04:52:13 +0200

    Update snapshots.

--- AppsDownloader/Properties/AssemblyInfo.cs
+++ AppsDownloader/Properties/AssemblyInfo.cs
@@ -20,4 +20,4 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("a3418f24-e739-45c2-b31e-f50f4124e600")]
 
-[assembly: AssemblyVersion("17.6.29.*")]
+[assembly: AssemblyVersion("17.7.18.*")]
--- AppsLauncher/Properties/AssemblyInfo.cs
+++ AppsLauncher/Properties/AssemblyInfo.cs
@@ -19,4 +19,4 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("ee806ac7-2a8b-4d79-8634-adbdbba0aebf")]
 
-[assembly: AssemblyVersion("17.6.29.*")]
+[assembly: AssemblyVersion("17.7.18.*")]
